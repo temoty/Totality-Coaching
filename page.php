608f8caaab113c960c3db2294f69466e9cabfe83
temoty <temoty@hotmail.com>
2017-12-29 22:56:35 -0500

    in page.php added .hentry class to article tag and removed article tag default class

--- page.php
+++ page.php
@@ -9,8 +9,9 @@
 						<main id="main" class="m-all t-2of3 d-5of7 cf" role="main" itemscope itemprop="mainContentOfPage" itemtype="http://schema.org/Blog">
                                        
 							<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+                            
 
-							<article id="post-<?php the_ID(); ?>" <?php post_class( 'cf' ); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
+							<article id="post-<?php the_ID(); ?>" class="hentry" <?php/* post_class( 'cf' ); */?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
 
 								<header class="article-header">
 
